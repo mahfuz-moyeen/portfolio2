855888125c9011f01acc76c2e3215ea1b3210509
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-06-08 21:52:48 +0600

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from "framer-motion";
+import TitleBar from '../TitleBar/TitleBar';
 
 const About = () => {
     return (
@@ -15,7 +16,41 @@ const About = () => {
             >
                 <div className="w-10/12 h-[700px] md:w-[35rem] md:h-[50rem] lg:w-[70rem] lg:h-[35rem] rounded-xl mx-auto bg-neutral overflow-y-auto">
                     <div>
+                        <div className='text-center my-5' >
+                            <p className='text-lg'>My Intro</p>
+                            <h1 className="text-4xl font-bold text-gray-200 my-3">
+                                About <span className='text-primary'>Me</span>
+                            </h1>
+                            <TitleBar />
+                        </div>
+                        <div className='grid grid-cols-1 lg:grid-cols-3 gap-2'>
+                            <div className='p-5'>
+                                <img className='rounded-2xl' src="https://avstechnolabs.com/Themeforest/Gaspar/01/Dark/assets/images/profile/wrap.jpg" alt="profile" />
+                            </div>
+                            <div className='lg:col-span-2 flex items-center'>
+                                <div>
+                                    <h1 className='text-left mb-10 text-2xl text-gray-200'>Personal <span className='text-primary'>Informations</span></h1>
+                                    <div className='grid grid-cols-2 gap-5 text-lg'>
+                                        <h1>First Name: <span className='text-gray-200'>Mahfuz Zahan</span></h1>
 
+                                        <h1>Email: <span className='text-primary'>mahfuzmoyeen01@gmail.com</span></h1>
+
+                                        <h1>Last Name: <span className='text-gray-200'>Moyeen</span></h1>
+
+                                        <h1>Phone: <span className='text-primary'>(+880)1703099425</span></h1>
+
+                                        <h1>Address: <span className='text-gray-200'>Rajshahi, Bangladesh</span></h1>
+
+                                        <h1>Languages: <span className='text-gray-200'>Bengli, English</span></h1>
+
+                                    </div>
+                                    <button className='my-5 btn btn-outline btn-accent'>Download Resume</button>
+                                    <div class="divider">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
